839aff5c196a0cfc57f187c4ac089f844965a9d7
nuokwan <994685563@qq.com>
2022-05-09 00:12:16 +0800

    update short grammar

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,18 @@ fn calculator2() {
     assert!(types_short::TermNumberParser::new().parse("((22)").is_err());
 }
 
+lalrpop_mod!(pub types_express);
+
+#[test]
+fn calculator3() {
+    assert!(types_express::TermNumParser::new().parse("22").is_ok());
+    assert!(types_express::TermNumParser::new().parse("(22)").is_ok());
+    assert!(types_express::TermNumParser::new().parse("212").is_ok());
+    assert!(types_express::TermNumParser::new().parse("((((22))))").is_ok());
+    assert!(types_express::TermNumParser::new().parse("(((((((22)))))))").is_ok());
+    assert!(types_express::TermNumParser::new().parse("((22)").is_err());
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
