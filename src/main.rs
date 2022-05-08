0e1eb856a606dc1552eb6eec76f02dcaaff71bf1
nuokwan <994685563@qq.com>
2022-05-08 14:36:14 +0800

    add lalrpop

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,18 @@ fn calculator1() {
     assert!(types::TermParser::new().parse("((22)").is_err());
 }
 
+lalrpop_mod!(pub types_short);
+
+#[test]
+fn calculator2() {
+    assert!(types_short::TermNumberParser::new().parse("22").is_ok());
+    assert!(types_short::TermNumberParser::new().parse("(22)").is_ok());
+    assert!(types_short::TermNumberParser::new().parse("212").is_ok());
+    assert!(types_short::TermNumberParser::new().parse("((((22))))").is_ok());
+    assert!(types_short::TermNumberParser::new().parse("(((((((22)))))))").is_ok());
+    assert!(types_short::TermNumberParser::new().parse("((22)").is_err());
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Hello, world!");
